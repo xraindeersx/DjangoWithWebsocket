16de9a270e07608bdbe6f547df7125c4dc42a8ac
Aleh Shved <aleh.shved@biviar.com>
2023-11-21 11:08:21 +0300

    add custom permission

--- cat_app/views.py
+++ cat_app/views.py
@@ -2,11 +2,12 @@ from rest_framework import viewsets
 from .models import Cat
 from .serializers import CatSerializer
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadOnly
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(breeder=self.request.user)
